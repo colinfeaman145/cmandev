6f91032d93c025eafb91b0f9028026dbc56a25be
colinfeaman145 <146300157+colinfeaman145@users.noreply.github.com>
2023-12-08 19:11:21 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  if (Math.random() < 0.8) {
+    window.location.replace("https://www.youtube.com/watch?v=JmCz_AE_x9g");
+  } else {
+    window.location.replace("https://www.youtube.com/watch?v=p7YXXieghto");
+  }
+};
